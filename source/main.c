091cce2bf50c3149f3217fe7beb5c92f91c3940f
Aem1024 <aem11607@gmail.com>
2024-03-14 01:19:27 -0600

    Refactored printing into function.

--- source/main.c
+++ source/main.c
@@ -13,28 +13,37 @@ struct enemy {
         int enemyY;
         int enemyAlive;
 };
+
+// DisplayPixel function
+void pixlOut(int x, int y, int foreground, int background, char tile) {
+
+
+	printf("\x1b[%i;%im",background,foreground);
+	// Sets colour to specified colours
+	printf("\x1b[%i;%iH", x, y);
+	printf("%c", tile);
+	// Sets back to white and black background
+	printf("\x1b[0m"); 
+}	
+
+
 // Main program entrypoint
-int main(int argc, char* argv[])
+int main()
 {
     //initializes console
     consoleInit(NULL);
     // Configure our supported input layout: a single player with standard controller styles
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
-
+    consoleSetWindow(NULL, 0, 0, COLUMNS, ROWS);
     struct enemy en;
-
     // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
     PadState pad;
     padInitializeDefault(&pad);
     padUpdate(&pad);
-    u64 kDown = padGetButtonsDown(&pad);
-    //TODO: Refactor to struct
-    // X at last frame and Y at last frame
     int movsafe = 1;
     int x = 1;
-    int y = 1;
+    int y = 2;
     int xlf, ylf;
-    int i, j;
     // Health and Score (Obviously)
     int health = 3;
     int score = 0;
@@ -73,62 +82,47 @@ int main(int argc, char* argv[])
         // Checks if moving is safe
         movsafe = 1;
 
-         if (map[y][x] >= 1) {
+         if (map[x][y] >= 1) {
             movsafe = 0;
             x = xlf;
             y = ylf;
         }
         // Iterating over the array
-        for (j = 0; j < COLUMNS; j++) {
-            for (i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLUMNS; j++) {
+            for (int i = 0; i < ROWS; i++) {
                 switch (map[i][j]) {
                     case 0 :
-                        // Change colour to green
-                        printf("\x1b[32m");
-                        printf("\x1b[%d;%dH%c", i, j, 219);
-                        printf("\x1b[0m");
+			// grass
+			pixlOut(i, j, 32, 0, 219);
                         break;
                     case 1 :
-                        // Change colour to blue
-                        printf("\x1b[37;44m");   // Set background to blue, foreground to white
-                        printf("\x1b[%d;%dH^", i, j); // Print the white tilde with blue background
-                        printf("\x1b[0m");           // Revert back to default colors (black and white)
+			// water
+
+			pixlOut(i, j, 37, 44, 94);
                         break;
                     case 2 :
-                        // bottom-right
-                        printf("\x1b[37;42m");
-                        printf("\x1b[%d;%dH%c", i, j, 201);
-                        printf("\x1b[0m");
+			// Top-Left
+			pixlOut(i, j, 37, 42, 201);
                         break;
                     case 3 :
+			pixlOut(i, j, 37, 42, 200);
                         // top-right
-                        printf("\x1b[37;42m");
-                        printf("\x1b[%d;%dH%c", i, j, 200);
-                        printf("\x1b[0m");
                         break;
                     case 4 :
                         // left-right
-                        printf("\x1b[37;42m");
-                        printf("\x1b[%d;%dH%c", i, j, 205);
-                        printf("\x1b[0m");
+			pixlOut(i, j, 37, 42, 205);
                         break;
                     case 5 :
                         // bottom-top
-                        printf("\x1b[37;42m");
-                        printf("\x1b[%d;%dH%c", i, j, 186);
-                        printf("\x1b[0m");
+			pixlOut(i, j, 37, 42, 186);
                         break;
                     case 6 :
                         // left-top
-                        printf("\x1b[37;42m");
-                        printf("\x1b[%d;%dH%c", i, j, 188);
-                        printf("\x1b[0m");
+			pixlOut(i, j, 37, 42, 188);
                         break;
                     case 7 : 
                         // bottom - left
-                        printf("\x1b[37;42m");
-                        printf("\x1b[%d;%dH%c", i, j, 187);
-                        printf("\x1b[0m");
+			pixlOut(i, j, 37, 42, 187);
                         break;
                     
                 }
@@ -140,22 +134,22 @@ int main(int argc, char* argv[])
         u64 kDown = padGetButtonsDown(&pad);
         if (kDown & HidNpadButton_Left && x > 1 && movsafe == 1) {
             if (map[y][x - 1] == 0) {
-                x = x - 1;
+                x -= 1;
             }
         }
         if (kDown & HidNpadButton_Right && x < COLUMNS && movsafe == 1) {
             if (map[y][x + 1] == 0) {
-                x = x + 1;
+                x += 1;
             }
         }
         if (kDown & HidNpadButton_Up && y > 2 && movsafe == 1) {
             if (map[y - 1][x] == 0) {
-                y = y - 1;
+                y -= 1;
             }
         }
         if (kDown & HidNpadButton_Down && y < ROWS && movsafe == 1) {
             if (map[y + 1][x] == 0) {
-                y = y + 1;
+                y += 1;
             }
 
         }
@@ -165,13 +159,11 @@ int main(int argc, char* argv[])
         if (kDown & HidNpadButton_B) {
             health = health - 1;
         }
-        printf("\x1b[37;42m");
-        printf("\x1b[%d;%dH%c", y, x, 2);
-        printf("\x1b[0m");
+
+	pixlOut(y, x, 37, 42, 2);
+
         if (en.enemyAlive == 1) {
-            printf("\x1b[31;42m");
-            printf("\x1b[%d;%dH%c", en.enemyY, en.enemyX, 2);
-            printf("\x1b[0m");
+	    pixlOut(en.enemyY, en.enemyX, 31, 42, 2);
         }
         if (kDown & HidNpadButton_Plus) {
             break;
